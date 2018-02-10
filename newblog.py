38b97c44052abc3e897e7dc4fffe86fa942ceae0
Earnest-Smith <edsdevs01@gmail.com>
2018-02-10 00:27:17 -0500

    new blog project
    new blog project including static and templates folders and files

--- newblog.py
+++ newblog.py
@@ -1,12 +1,18 @@
 import os
+import re
+from string import letters
+
 import jinja2
 import webapp2
+
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+##### Initial Blog handler function from template
 
-class Handler(webapp2.RequestHandler):
+class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -17,9 +23,74 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class MainPage(Handler):
+    def render_post(response, post):
+        response.out.write('<b>' + post.subject + '</b><br>')
+        response.out.write(post.content)
+
+class MainPage(BlogHandler):
     def get(self):
-        self.write("/ascii/")
+        self.write("Here is your new Blog! ")
 
 
-app = webapp2.WSGIApplication([ ('/', MainPage), ], debug=True)
+########## blog stuff
+
+######## blog parent key configuration
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+##### list of properties that blog has
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+####### Post render function that renders the blog entry in post html
+def render(self):
+    self._render_text = self.content.replace('/n', '<br>')
+    return render_str("post.html", p = self)
+
+##### Front page of Blog
+class BlogFront(BlogHandler):
+    def get(self):
+        posts = Post.all().order('-created')
+        self.render('front.html', posts = posts)
+
+####### Post page, page for individual posts
+class PostPage(BlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        posts = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+###### New Post page, page for creating new posts
+class NewPost(BlogHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "subject and content is required !"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+
+app = webapp2.WSGIApplication([ ('/', MainPage),
+                                ('/blog/?', BlogFront),
+                                ('/blog/([0-9]+)', PostPage),
+                                ('/blog/newpost', NewPost),
+                                ],
+                                debug=True)
